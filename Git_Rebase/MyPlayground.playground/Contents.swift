028d71e57418a5767c5d802af34af675d1398adb
Shreesha <shreesha.kedilaya@ymedialabs.com>
2017-01-12 20:37:32 +0530

    - Added the fifth Commit.

--- Git_Rebase/MyPlayground.playground/Contents.swift
+++ Git_Rebase/MyPlayground.playground/Contents.swift
@@ -9,4 +9,8 @@ var secondCommit = "firstCommit"
 
 var thirdCommit = "firstCommit"
 
+<<<<<<< 0665dd44c86a54ad2a24cc80a63a860ce2cd81a6
 var fourthCommit = "firstCommit"
+=======
+var fifthCommit = "fifthCommit"
+>>>>>>> - Added the fifth Commit.
